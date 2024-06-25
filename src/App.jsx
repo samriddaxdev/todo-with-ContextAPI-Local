cf285845684f7f3aab438c4270eb9a5971baf9bf
Samridda Paudel <84239594+samriddaxdev@users.noreply.github.com>
2024-06-25 20:33:12 +0545

    used map and a logic to toggle complete or check mark on the todo list which is completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
   };
 
-  const updatedTodo = (id, todo) => {
+  const updateTodo = (id, todo) => {
     setTodos((prev) =>
       prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)),
     );
@@ -18,9 +18,19 @@ function App() {
     setTodos((prev) => prev.filter((todo) => todo.id !== id));
   };
 
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo,
+      ),
+    );
+  };
+
   return (
     <TodoProvider
-      value={{ todos, addTodo, updatedTodo, deleteTodo, toggleComplete }}
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
     >
       <div className='bg-[#172842] min-h-screen py-8'>
         <div className='w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white'>
